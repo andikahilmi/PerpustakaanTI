ef642bab092e1dd5b9e2760fa14086bb3ad91868
andikahilmi <andikahilmi38@gmail.com>
2024-12-18 19:33:41 +0700

    Update Perpus.py

--- Perpus.py
+++ Perpus.py
@@ -37,7 +37,7 @@ def display_book(book):
 
 # Fungsi login
 def login_page():
-    st.title("Login")
+    st.title("Login Portal Perpustkaan")
     # Inisialisasi session_state untuk menyimpan kredensial
     if "credentials" not in st.session_state:
         st.session_state["credentials"] = {
